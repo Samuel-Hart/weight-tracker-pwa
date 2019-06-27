61af163693b8fba742e9e1cefc29bfbf0766e683
Samuel Hart <Samuel-Hart@users.noreply.github.com>
2019-06-27 14:00:20 -0500

    rebaseline

--- sw.js
+++ sw.js
@@ -1,25 +1,25 @@
-var cacheName = 'wt-start-page';
+var cacheName = 'hello-pwa';
 var filesToCache = [
-    '/',
-    '/index.html',
-    '/wt.css'
+  '/',
+  '/index.html',
+  '/css/style.css',
+  '/js/main.js'
 ];
+
+/* Start the service worker and cache all of the app's content */
 self.addEventListener('install', function(e) {
-    console.log('[ServiceWorker] Install');
-    e.waitUntil(
-        caches.open(cacheName).then(function(cache) {
-            console.log('[ServiceWorker] Caching app shell');
-            return cache.addAll(filesToCache);
-        })
-    );
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
 });
-self.addEventListener('activate', event => {
-    event.waitUntil(self.clients.claim());
-});
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request, { ignoreSearch: true }).then(response => {
-            return response || fetch(event.request);
-        })
-    );
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
 });

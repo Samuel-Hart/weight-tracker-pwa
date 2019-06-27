0448688f78bc399ddd2a655798c10b231ccda68c
Samuel Hart <Samuel-Hart@users.noreply.github.com>
2019-06-27 14:35:03 -0500

    service worker scope fix
    maybe?

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
 window.onload = () => {
-  'use strict';
+    'use strict';
 
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-             .register('./sw.js')
-             .then(function() { console.log('Service Worker Registered'); });
-  }
-}
+    if ('serviceWorker' in navigator) {
+        // Register a service worker hosted at the root of the
+        // site using a more restrictive scope.
+        navigator.serviceWorker.register('/sw.js', { scope: './' }).then(function(registration) {
+            console.log('Service worker registration succeeded:', registration);
+        }, /*catch*/ function(error) {
+            console.log('Service worker registration failed:', error);
+        });
+    } else {
+        console.log('Service workers are not supported.');
+    }
+};
